cd2c38782002cf09be05c1b6020541e959c9dfb8
alfonsogarcia-git <alfonsogarcia.ms@gmail.com>
2021-02-16 00:00:43 +0100

    Added files and exercises

---
+++ vector.R
@@ -0,0 +1,21 @@
+## Vector exercises: 3, 6, 7, 8, 9, 10, 12, 13, 14, 16, 20, 23
+
+# Exercise 3
+vect <- vector() # Empty vector
+vect <- c() # Another way to create an empty vector
+vect <- append(vect, c(6, 7, 8)) # appending values to vect
+vect
+
+# Exercise 6 
+vect <- c(1, 4, 7, 9, 12, 22, 0)
+print('Mean:')
+print(mean(vect))
+print('Sum:')
+print(sum(vect))
+print('Product:')
+print(prod(vect))
+
+# Exercise 7
+vect <- c(1, 4, 7, 9, 12, 22, NaN, NaN) 
+vect
+
